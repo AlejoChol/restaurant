936bd0a974866394f0d313766f77f045d8194e83
BrunoFormichella <formichellabruno@gmail.com>
2019-11-20 16:03:03 -0300

    Posible fix a show_database

--- show_database.php
+++ show_database.php
@@ -15,19 +15,23 @@
 	$dbpass = 'poli1';
 	$dbname = 'poli_dos';
 	$conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+	msquli_select_db($conn,"restaurant");
 
 	//Consigue todos los pedidos No entregados
 	$result = mysqli_query($conn,"SELECT * FROM pedidos WHERE entregado='No'");
 
 	//Arma un array de todos los Id de pedidos no entregados, para pedir sus detalles respectivos
+	
 	while ($row = $result->fetch_assoc()){
 		$Id_pedido_noEntregado[]=$row['Id_pedido'];
 	}
 
 	//Pide los detalles de cada pedido no entregado
+	
 	$result2 = mysqli_query($conn,'SELECT * FROM pedido_detalle WHERE Id_pedido IN (' . implode(',', array_map('intval', $Id_pedido_noEntregado)) . ')');
 
 	//Array tridimensional, donde primer eje es el Id_pedido, el segundo es item y tercero es cada espicificacion del item
+	
 	$ID_pedidoActual=0;
 	while ($row = $result2->fetch_assoc()){ //Nombre, Cant, Precio
 		if($ID_pedidoActual != $row['Id_pedido']){
@@ -38,8 +42,11 @@
 		$pedidos_detalles[$ID_pedidoActual][$numeroItem]['Precio']	= $row['Precio'];
 		$numeroItem++;
 	}
+	
 	$result = mysqli_query($conn,"SELECT * FROM pedidos WHERE entregado='No'");
+	
 	while ($row = $result->fetch_assoc()){
+		$ID_pedidoActual=$row['Id_pedido'];
 		echo "<table style='width:100%' class='interact'><tr>" .
 			'<td>' . $row['Id_pedido']	 . '</td>' .
 			'<td>' . $row['Id_mesa']	 . '</td>' .
@@ -50,9 +57,9 @@
 		echo "<table class='collapsible'style='width:90%'>"; 
 		for ($i = 0; $i < count($pedidos_detalles[$i]); $i++){
 			echo '<tr>' .
-				'<td>' . $pedidos_detalles[$row['Id_pedido']][$i]['Nombre'] . '</td>' .
-				'<td>' . $pedidos_detalles[$row['Id_pedido']][$i]['Cant'] . '</td>' .
-				'<td>' . $pedidos_detalles[$row['Id_pedido']][$i]['Precio'] . '</td>' .
+				'<td>' . $pedidos_detalles[$ID_pedidoActual][$i]['Nombre'] . '</td>' .
+				'<td>' . $pedidos_detalles[$ID_pedidoActual][$i]['Cant'] . '</td>' .
+				'<td>' . $pedidos_detalles[$ID_pedidoActual][$i]['Precio'] . '</td>' .
 			'</tr></table>';
 		}
 	}
